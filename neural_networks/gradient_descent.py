34d744a2eff23fb69bce13c77d0ba5683b33574c
ananinNS <mailananin@mail.ru>
2022-09-19 18:52:03 +0400

    Рассказал Сереге о классах

--- neural_networks/gradient_descent.py
+++ neural_networks/gradient_descent.py
@@ -1,4 +1,31 @@
 # -*- coding: utf-8 -*-
 # Реализован алгоритм градиентного спуску.
 
+str = "Привет мир"
 
+for v in str:
+    print(v, end=", ")
+
+class MyClass:
+    a1 = "Привет"
+    a2 = 2
+
+
+b = MyClass()
+
+print(MyClass.a1)
+MyClass.a1 = "111141"
+b.a1 = "7777"
+b.ss = 123
+print(b.a1, MyClass.a1, b.ss)
+
+
+def gene(start_value):
+    start_value += 1
+    yield start_value
+
+
+b = gene(2)
+
+print(type(b))
+print(b.__next__())
